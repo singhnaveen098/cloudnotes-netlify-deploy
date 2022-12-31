05a2812b2f6664e7860459b63f708145e45e03a9
naveen098 <singh.naveen098@gmail.com>
2022-12-31 12:46:00 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI =
-  "mongodb://0.0.0.0:27017/cloudnotes?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-// const mongoURI=process.env.ATLAS
+const mongoURI=process.env.ATLAS
 
 const connecttomongo = () => {
   mongoose.connect(mongoURI, { keepAlive: true }, () =>
